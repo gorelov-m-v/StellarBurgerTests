e239a6716059e1c933887d395ef8cfa09f33e01c
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-03-20 22:44:37 +0300

    Собрал Дата провайдер Junit для тестов на регистрацию

--- ApiTests/src/test/java/tests/user/UserRegistrationTests.java
+++ ApiTests/src/test/java/tests/user/UserRegistrationTests.java
@@ -1,5 +1,8 @@
 package tests.user;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import io.restassured.RestAssured;
 import model.data.User;
 import model.requests.user.UserDeletionRequest;
@@ -8,9 +11,11 @@ import model.responses.user.registration.RegistrationResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 
 import static org.assertj.core.api.Assertions.*;
-
+@RunWith(DataProviderRunner.class)
 public class UserRegistrationTests extends TestHelper {
     UserRegistrationRequest registrationRequest = new UserRegistrationRequest();
     RegistrationResponse registrationResponse;
@@ -21,11 +26,33 @@ public class UserRegistrationTests extends TestHelper {
         RestAssured.baseURI = urls.getStellarBurgerProd();
     }
 
+
+    @DataProvider
+    public static Object[][] data() {
+    return new Object[][] {
+            {1,  true},
+            {2,  true},
+            {0, false}
+        };
+    }
+
     @Test
-    public void smokeUserRegistrationTest() {
+    @UseDataProvider("data")
+    public void smokeUserRegistrationTest(int length, boolean success) {
         User requestedUser = new User().withEmail(generate.randomEmail())
-                                       .withPassword(generate.randomPassword(8))
+                                       .withPassword(generate.randomPassword(length))
                                        .withName(generate.randomName());
+
+        registrationResponse = registrationRequest.userRegistration(requestedUser);
+
+        assertThat(registrationResponse.success()).isEqualTo(success);
+    }
+    @Test
+    public void smokeUserRegisterationTest2() {
+
+        User requestedUser = new User().withEmail(generate.randomEmail())
+                .withPassword(generate.randomPassword(8))
+                .withName(generate.randomName());
 
         registrationResponse = registrationRequest.userRegistration(requestedUser);
 
